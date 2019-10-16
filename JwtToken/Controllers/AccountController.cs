89e5b267a7f523db627f32cdc3494ea698192d29
shaikh.mohsin <shaikh.mohsin@mobiliya.com>
2019-10-16 16:27:58 +0530

    Add project files.

---
+++ JwtToken/Controllers/AccountController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using JwtToken.Dtos;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JwtToken.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountController : ControllerBase
+    {
+        private readonly AccountService _accountService;
+
+        public AccountController(AccountService accountService)
+        {
+            _accountService = accountService;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        [HttpPost]
+        public ActionResult Login(LoginDto loginDto)
+        {
+            var jwtToken = _accountService.Login(loginDto);
+
+            if (jwtToken == null)
+            {
+                return Unauthorized();
+            }
+
+            return Ok(jwtToken);
+        }
+    }
+}
---
+++ JwtToken/Controllers/InvoiceController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+
+
+//only for authorized and admin roll : https://localhost:5001/api/invoice/admin
+//for authorized admin and accountant role : https://localhost:5001/api/invoice/admin-acc
+//for unauthorized and anonymouse anonymouse : https://localhost:5001/api/invoice/all
+
+namespace JwtToken.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class InvoiceController : ControllerBase
+    {
+        [HttpGet("all")]
+        public IActionResult View()
+        {
+            return Ok("You can view invoices!");
+        }
+
+        [Authorize(Roles = "Administrator,Accountant")]
+        [HttpGet("admin-acc")]
+        public IActionResult Create()
+        {
+            var userIdClaim = HttpContext.User.Claims.Where(x => x.Type == "userid").SingleOrDefault();
+            return Ok($"Your User ID is {userIdClaim.Value} and you can create invoices!");
+        }
+
+        [Authorize(Roles = "Administrator")]
+        [HttpGet("admin")]
+        public IActionResult Delete()
+        {
+            var userIdClaim = HttpContext.User.Claims.Where(x => x.Type == "userid").SingleOrDefault();
+            return Ok($"Your User ID is {userIdClaim.Value} and you can delete invoices!");
+        }
+    }
+}
